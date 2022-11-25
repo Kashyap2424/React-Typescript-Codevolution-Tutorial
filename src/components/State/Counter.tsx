7cbdf44cd4ed16c9cf8af4901153b01b434cc754
Kashyap2424 <kapdikashyap2003@gmail.com>
2022-11-25 08:31:55 +0530

    Tutorial #13 useReducer Strict Action Types.

--- src/components/State/Counter.tsx
+++ src/components/State/Counter.tsx
@@ -4,10 +4,16 @@ type CounterState = {
   count: number;
 };
 
-type CounterAction = {
-  type: string;
+type UpdateAction = {
+  type: "increment" | "decrement";
   payload: number;
 };
+
+type ResetAction = {
+  type: "reset";
+};
+
+type CounterAction = UpdateAction | ResetAction;
 
 const initialState = { count: 0 };
 
@@ -35,6 +41,7 @@ const Counter = () => {
       <button onClick={() => dispatch({ type: "decrement", payload: 10 })}>
         Decrement 10
       </button>
+      <button onClick={() => dispatch({ type: "reset" })}>Reset</button>
     </div>
   );
 };
